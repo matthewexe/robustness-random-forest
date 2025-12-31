cafb904ee4f8e5d630cd59bf0ae0590b8a520135
matthewexe <matteobaldi233@gmail.com>
2025-12-31 16:24:56 +0100

    feat(config): rf path and diagram size added

--- src/robustness/domain/config.py
+++ src/robustness/domain/config.py
@@ -1,6 +1,14 @@
+from dataclasses import dataclass
+from os import PathLike
+from pathlib import Path
+
 from robustness.domain.utils.singleton import Singleton
 
+root_path = Path().parent.parent.parent.parent
 
+@dataclass
 class Config(Singleton):
-    diagram_size: int
-    prefix_var: str
+    diagram_size: int = 200
+    prefix_var: str = "t_"
+    dataset_name: str = "Meat"
+    rf_path: str | PathLike[str] = root_path / "Random_Forest_Aeon_Univariate/results"
